c1cf6323835693564cb74304c2a116f66527a2e2
muhonja <linahmuhonjaimbari>
2024-03-21 15:30:28 +0300

    add create object for register page

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, request, session, redirect, url_for,flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = "kjhfgdjkouae"
 app.permanent_session_lifetime = timedelta(days=7)
 
 #creating the database connection
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3 '
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.sqlite3 '
 app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class users(db.Model):
+class User(db.Model):
     _id  = db.Column(db.Integer, primary_key = True)
     fname = db.Column(db.String(100))
     lname = db.Column(db.String(100))
@@ -32,7 +33,7 @@ def login():
         session.permanent = True
         # x = request.form
         # print(x)
-        email = request.form["email"]
+        email = request.form.get("email")
         # session["email"] = email        
         password = request.form.get("password")
         if len(email) < 12:
@@ -40,7 +41,7 @@ def login():
         elif len(password) < 8:      
             flash("Password too short")
         else:
-            user = users.query.filter_by(name=email).first()
+            user = User.query.filter_by(name=email).first()
             if user and user.password == password:
                 session["email"] = email
                 return redirect(url_for("index"))
@@ -60,38 +61,46 @@ def index():
 
 @app.route("/register", methods=["POST","GET"])
 def register():
+    
+    email = request.form.get("email")
+    session["email"] = email
+    # print(session["email"])
+    fname= request.form.get("fname")
+    lname= request.form.get("lname")
+    password = request.form.get("password")
+    email = request.form.get("email")
+    confirmPassword = request.form.get("confirmPassword")
     if request.method == "POST":
-        session.permanent = True
+        # session.permanent = True
         # user = request.form
-        # print(user)
-        email = request.form["email"]
-        session["email"] = email
-        # print(session["email"])
-        fname= request.form.get("fname")
-        lname= request.form.get("lname")
-        password = request.form.get("password")
-        email = request.form.get("email")
-        confirmPassword = request.form.get("confirmPassword")
+        # print(user)   
         if len(fname) <= 1:
             flash("please enter more than one character", category="error")
         elif len(lname) < 1:
             flash("please enter more than one character", category="error")
         elif len(email) < 8:
-          flash("Email too short")        
-        elif password != confirmPassword:
-            flash("Password don't match", category="error")
+          flash("Email too short") 
         elif len(password) <  8:
-            flash("Password is too short!", category="error")
+            flash("Password is too short!", category="error")       
+        elif password != confirmPassword:
+            flash("Password don't match", category="error")        
         else:
-            user = request.form
-            usr = users(user)
-            email = usr.email
-            if users.query.filter_by(name = email).all().count() > 0:
-                flash("Account already exists!")
-            else:                            
+            usr = User(fname=fname,
+                       lname=lname, 
+                       email=email, 
+                       password=password, 
+                       confirmPassword=confirmPassword
+                       )
+            create_user =  User.query.filter_by(email = email).count()
+            if create_user > 1:
+                flash("Account already exists! Kindly create account")
+                
+                return render_template("register.html")
+            else:
                 db.session.add(usr)
                 db.session.commit()
-                return redirect(url_for("index"))
+                return redirect(url_for("index"))            
+            return redirect(url_for("index"))
     return render_template("register.html")
 
 @app.route("/logout")
@@ -103,5 +112,6 @@ def logout():
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
+        # db.drop_all()
+        db.create_all()        
     app.run(debug = True)
